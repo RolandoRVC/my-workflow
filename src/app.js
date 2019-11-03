2977296b6d34364151d5fc3e1ab89d2d18f9989b
Rolando Velazquez <velazquezchirolando@gmail.com>
2019-11-03 02:05:11 -0500

    update

--- src/app.js
+++ src/app.js
@@ -1,3 +1,9 @@
 import "./styles/normalize.css";
 import "./styles/main.scss";
 import "./static/logo.svg";
+
+const concole = ()=>{
+let mimensaje = "hola";
+    console.log(mimensaje)
+}
+concole();
