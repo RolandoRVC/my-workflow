f4e0476470509a0e5fd0cb7148efd71687037796
Rolando Velazquez <velazquezchirolando@gmail.com>
2019-11-03 01:29:20 -0500

    first commit

---
+++ webpack/webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const autoprefixer = require('autoprefixer');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, '../dist'),
+        filename: 'js/bundle.js'
+    },
+    devtool: 'source-map',
+    devServer: {
+        port: 5000,
+        open: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.hbs$/,
+                loader: 'handlebars-loader'
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            autoprefixer: {
+                                browser: ["last 2 versions"]
+                            },
+                            plugins: () => [
+                                autoprefixer
+                            ]
+                        }
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|png|gif|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'static/',
+                            useRelativePath: true
+                        }
+                    },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: '65-90',
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.hbs',
+            minify: {
+                html5: true,
+                collapseWhitespace: true,
+                caseSensitive: true,
+                removeComments: true,
+                removeEmptyElements: true
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/[name][hash].css",
+            chunkFilename: "[id].css"
+        })
+    ]
+};
